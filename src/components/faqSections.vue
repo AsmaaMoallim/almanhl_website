58ba32249d165e5dc9925ea959bd154f0d15cf2e
Asmaa Moallim <41068564+AsmaaMoallim@users.noreply.github.com>
2023-03-27 09:04:52 +0300

    animate on scroll

--- src/components/faqSections.vue
+++ src/components/faqSections.vue
@@ -1,15 +1,16 @@
 <template>
     <main class="faqSections">
-        <div class="sections">
-            <a href="#" @click="section = 'section_1'">General</a>
+
+        <div class="sections" data-aos="fade-up">
+            <a href="" @click.prevent="section = 'section_1'">General</a>
             <span class="seperation_line"></span>
-            <a href="#" @click="section = 'section_2'">Pre-ICO & ICC</a>
+            <a href="" @click.prevent="section = 'section_2'">Pre-ICO & ICC</a>
             <span class="seperation_line"></span>
-            <a href="#" @click="section = 'section_3'">ICO Tokens</a>
+            <a href="" @click.prevent="section = 'section_3'">ICO Tokens</a>
             <span class="seperation_line"></span>
-            <a href="#" @click="section = 'section_4'">Legal</a>
+            <a href="" @click.prevent="section = 'section_4'">Legal</a>
         </div>
-        <div class="question_section">
+        <div class="question_section" data-aos="fade-up">
             <div class="question">
                 <div v-if="section == 'section_1'">
                     <!-- <h1>section_1</h1> -->
@@ -49,7 +50,7 @@
                 <div v-if="section == 'section_4'">
                     <!-- <h1>section_4</h1> -->
 
-                    <Question  id="1" question_prompt='What is cryptocurrency?' arrow_direction='false'
+                    <Question id="1" question_prompt='What is cryptocurrency?' arrow_direction='false'
                         question_answer="The best cryptocurrency to buy is one we are willing to hold onto even if it goes down. For example, I believe in Steem enough that I am willing to hold it even if it goes down 99% and would start buying more of it if the price dropped." />
                     <Question id="1" question_prompt='What is cryptocurrency?' arrow_direction='false'
                         question_answer="The best cryptocurrency to buy is one we are willing to hold onto even if it goes down. For example, I believe in Steem enough that I am willing to hold it even if it goes down 99% and would start buying more of it if the price dropped." />
